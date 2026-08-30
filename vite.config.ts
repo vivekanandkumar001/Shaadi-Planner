8e7032bfe02f1b288dabc663f203bdbdd9477573
Vivekananda0010 <vivekanandkumar865124@gmail.com>
2026-08-30 14:03:04 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -84,7 +84,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Shaadi Planner"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
